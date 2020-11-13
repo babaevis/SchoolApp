894bbb42196b7a34e5babcfb24ad364fcca7f9b6
Islam Babaev <babaevisha@gmail.com>
2020-11-13 09:27:17 +0300

    main

--- src/main/java/com/babaev/Main.java
+++ src/main/java/com/babaev/Main.java
@@ -12,7 +12,7 @@ import java.sql.Connection;
 import java.util.List;
 
 class Main {
-private static final String SCRIPT = "src/main/resources/tables.sql";
+    private static final String SCRIPT = "src/main/resources/tables.sql";
 
     public static void main(String[] args) {
         Connection con = ConnectionProvider.getConnection();
