713ce0404e4d965cc67de34a37cd4579d2652bf4
Islam Babaev <babaevisha@gmail.com>
2020-11-14 07:50:59 +0300

    wef

--- src/main/java/com/babaev/Main.java
+++ src/main/java/com/babaev/Main.java
@@ -12,7 +12,7 @@ import java.sql.Connection;
 import java.util.List;
 
 class Main {
-    private static final String SCRIPT = "src/main/resources/tables.sql";
+    private static final String SCRIPT     = "src/main/resources/tables.sql";
     private static final String PROPERTIES = "src/main/resources/db.properties";
 
     public static void main(String[] args) {
